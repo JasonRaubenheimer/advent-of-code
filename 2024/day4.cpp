b4257dea9f2f331cee273c6b36a10d655a9cffc8
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2024-12-07 20:29:07 +0000

    day 4 done!

--- 2024/day4.cpp
+++ 2024/day4.cpp
@@ -145,6 +145,52 @@ void print(const size_t &x_pos, const size_t &y_pos, const std::string &dir)
     // std::cout << x_pos << "," << y_pos << "  " << dir << std::endl;
 }
 
+bool is_x_mas(const std::vector<std::string> &grid, const size_t &x_pos, const size_t &y_pos)
+{
+    // check if possible... needs to be one in from each edge
+    if (x_pos < 1 || x_pos > grid.at(y_pos).length() - 2)
+    {
+        // 0123   len = 4    len-2 = 2   xpos_max = 2
+        return false;
+    }
+    if (y_pos < 1 || y_pos > grid.size() - 2)
+    {
+        return false;
+    }
+
+    if (grid.at(y_pos).at(x_pos) != 'A')
+    {
+        return false;
+    }
+
+    // top left 'M' then bottom right 'S'
+    // top right 'M' then bottom left 'S'
+    // bottom right 'M' then top left 'S'
+    // bottom left 'M' then top right 'S'
+
+    char top_left{grid.at(y_pos - 1).at(x_pos - 1)};
+    char top_right{grid.at(y_pos - 1).at(x_pos + 1)};
+    char bottom_right{grid.at(y_pos + 1).at(x_pos + 1)};
+    char bottom_left{grid.at(y_pos + 1).at(x_pos - 1)};
+
+    size_t xmas_count{0};
+    if ((top_left == 'M' && bottom_right == 'S') || (top_left == 'S' && bottom_right == 'M'))
+    {
+        ++xmas_count;
+    }
+    if ((top_right == 'M' && bottom_left == 'S') || (top_right == 'S' && bottom_left == 'M'))
+    {
+        ++xmas_count;
+    }
+
+    if (xmas_count == 2)
+    {
+        return true;
+    }
+
+    return false;
+}
+
 int main()
 {
     std::cout << "day 4" << std::endl;
@@ -161,6 +207,7 @@ int main()
     }
 
     size_t xmas_count{0};
+    size_t xmas_count_p2{0};
 
     for (size_t y{0}; y < grid.size(); ++y)
     {
@@ -206,10 +253,17 @@ int main()
                 ++xmas_count;
                 print(x, y, "nw");
             }
+
+            // part 2
+            if (is_x_mas(grid, x, y))
+            {
+                ++xmas_count_p2;
+            }
         }
     }
 
-    std::cout << "\nxmas_count: " << xmas_count << std::endl;
+    std::cout << "\nxmas_count_p1: " << xmas_count << std::endl;
+    std::cout << "\nxmas_count_p2: " << xmas_count_p2 << std::endl;
 
     return 0;
 }
